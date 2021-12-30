308ba9b033e7e9a07efc0d6ec7fc5db660adb362
anish-lkakapragada <anish.lakkapragada@gmail.com>
2021-12-30 09:55:57 -0800

    release please

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 
+import sys
+
+version_name = sys.argv[1].replace("refs/tags/", "")
+del sys.argv[1]
+
 setup(
-    name="countloc",
-    version="0.1",
-    packages=['countloc'],
-    entry_points={
+    name = 'countloc',
+    version="0.67", 
+    author = "Anish Lakkapragada", 
+    author_email="anish.lakkapragada@gmail.com",
+    description = "countloc is a fun application to count the loc in ur dir", 
+    keywords=["Fun"], 
+    packages = ['countloc'],
+    entry_points = {
         'console_scripts': [
             'countloc = countloc.__main__:main'
         ]
-    }
-)
+    })
