e1116c4a57fa1fba477afdb5dc00f3b0f6c9900a
anish-lkakapragada <anish.lakkapragada@gmail.com>
2021-12-30 10:11:01 -0800

    finished?

--- setup.py
+++ setup.py
@@ -5,14 +5,20 @@ import sys
 version_name = sys.argv[1].replace("refs/tags/", "")
 del sys.argv[1]
 
+with open("README.md", "r") as fh:
+    read_me_description = fh.read()
+
 setup(
     name = 'countloc',
-    version="0.67", 
+    version="0.69", 
     author = "Anish Lakkapragada", 
     author_email="anish.lakkapragada@gmail.com",
-    description = "countloc is a fun application to count the loc in ur dir", 
-    keywords=["Fun"], 
+    description = "countloc is a simple CLI to count the number of lines of code in your directory that you wrote.", 
+    keywords=["LOC", "SLOC", "CLI"], 
     packages = ['countloc'],
+    license="MIT",
+    long_description = read_me_description, 
+    long_description_content_type="text/markdown",
     entry_points = {
         'console_scripts': [
             'countloc = countloc.__main__:main'
